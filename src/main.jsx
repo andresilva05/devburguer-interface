47bb16326b3cb7e6a07073095bcf1c08e2454271
andresilva05 <andresilva9657@gmail.com>
2024-11-14 21:18:09 -0300

    feat: adiciona GlobalStyles para estilização global da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import GlobaStyles from './styles/globalStyles';
 import { Login } from './components/Login';
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<Login />
-	</StrictMode>,
+  <StrictMode>
+    <Login />
+    <GlobaStyles />
+  </StrictMode>,
 );
