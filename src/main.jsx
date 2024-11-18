a2d67118aaa39be4256072fc3b0881736031f506
andresilva05 <andresilva9657@gmail.com>
2024-11-17 21:53:07 -0300

    Refactor: Ajustado caminho de importação do componente e ordem de renderização

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import GlobaStyles from './styles/globalStyles';
-import { Login } from './components/Login';
+import { Login } from './containers/Login ';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
     <GlobaStyles />
+    <Login />
   </StrictMode>,
 );
