5dff3c301a76dab1e66bff3048703b196349cda6
andresilva05 <andresilva9657@gmail.com>
2024-11-19 22:29:44 -0300

    Estiliza containers e ajusta layout do login: fundo, fontes e espaçamento

--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -1,17 +1,96 @@
 import styled from 'styled-components';
+import BackgroundLogin from '../../assets/background-login.png';
+import Background from '../../assets/background.png';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
 
-export const LeftContainer = styled.div``;
+`;
 
-export const RightContainer = styled.div``;
+export const LeftContainer = styled.div`
+    background:  url('${BackgroundLogin}');
+    background-size: cover;
+    background-position: center;
 
-export const Title = styled.h2``;
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-export const Form = styled.form``;
+    img{
+        width: 80%;
+    }
+`;
 
-export const InputContainer = styled.div``;
+export const RightContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-export const Link = styled.a``;
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
 
-export const Button = styled.button``;
+    background:  url('${Background}');
+    background-color: #1E1E1E;
+    background-size: cover;
+
+    p{
+        color: #ffff;
+        font-size: 18px;
+        font-weight: 800;
+
+        a{
+            text-decoration: underline;
+            color: #ffff;
+        }
+    }
+`;
+
+export const Title = styled.h2`
+ font-family: "Road Rage", sans-serif;
+    font-size: 40px;
+    color: #ffff;
+
+    span {
+
+        color: #9758a6 ;
+        font-family: "Road Rage", sans-serif;
+    }
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px;
+    width: 100%;
+    max-width: 400px;
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: 100%;
+
+    input {
+        width: 100%;
+        border: none;
+        height: 52px;
+        border-radius: 5px;
+        padding: 0 16px;
+    }
+
+    label {
+        font-size: 18px;
+        font-weight: 600;
+        color: #ffff;
+
+    }
+`;
