a29172ebbd25dbf0c244d89e999b88f61c13d878
andresilva05 <andresilva9657@gmail.com>
2024-11-19 22:28:19 -0300

    Estiliza o botão com efeito hover e borda personalizada

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ContainerButton = styled.button`
+    width: 100%;
+    height: 52px;
+    border: 0;
+    border-radius: 5px;
+    background-color: #9758A6;
+    font-size: 30px;
+    font-family: "Road Rage", sans-serif;
+    color: #ffff;
+    
+    &:hover {
+        background-color: #6F3576 ;
+        background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23333' stroke-width='3' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+        border: 1px dashed #ffff;
+    }
+
+`;
