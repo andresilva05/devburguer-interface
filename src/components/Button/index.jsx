8553b6347cfa0a0fb660c1e3386e3fc8ca8ca5bc
andresilva05 <andresilva9657@gmail.com>
2024-11-23 19:25:35 -0300

    feat: torna o componente Button mais flexível
    - Adicionado o operador spread (...props) para aceitar e repassar propriedades adicionais ao ContainerButton.
    - Mantida a validação de propTypes para a propriedade children.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 import { ContainerButton } from './styles';
 
-export function Button({ children }) {
-	return <ContainerButton>{children}</ContainerButton>;
+export function Button({ children, ...props }) {
+  return <ContainerButton {...props}>{children}</ContainerButton>;
 }
 
 Button.propTypes = {
-    children: PropTypes.string,
-}
+  children: PropTypes.string,
+};
