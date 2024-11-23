5bef5ef901e59926828d83185afe56a4666d892e
andresilva05 <andresilva9657@gmail.com>
2024-11-23 19:28:26 -0300

    feat(register): página de cadastro funcional com validação e UX aprimorada
    - Implementada validação de campos com React Hook Form e Yup.
    - Conectada à API para criação de usuários, com tratamento de status:
      - Sucesso: Redireciona para a página de login com mensagem positiva.
      - Conflito: Exibe mensagem informando e-mail já cadastrado.
      - Erro genérico: Notifica falha no sistema.
    - Adicionado feedback ao usuário com Toastify para melhor experiência.
    - Estilização consistente e alinhada com os componentes já existentes.

---
+++ src/containers/Register/index.jsx
@@ -0,0 +1,118 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import * as yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
+import Logo from '../../assets/logo.png';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+import {
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+  Link,
+} from './styles';
+
+export function Register() {
+  const navigate = useNavigate()
+  const schema = yup
+    .object({
+      name: yup.string().required('O nome é obrigatório'),
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+        .required('Confirme sua senha'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const { status } = await api.post(
+        '/users',
+        {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        },
+        {
+          validateStatus: () => true,
+        },
+      );
+      if (status === 200 || status === 201) {
+        setTimeout(() => {
+          navigate('/login')
+        },2000)
+        toast.success('Conta criada com sucesso');
+      } else if (status === 409) {
+        toast.error('E-mail já cadastrado! Faça o login para continuar');
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error('😭 Falha no Sistema! Tente novamente');
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <LeftContainer>
+          <img src={Logo} alt="logo-dev-burguer" />
+        </LeftContainer>
+        <RightContainer>
+          <Title> Criar Conta</Title>
+
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <InputContainer>
+              <label htmlFor="">Nome</label>
+              <input type="text" {...register('name')} />
+              <p>{errors?.name?.message}</p>
+            </InputContainer>
+            <InputContainer>
+              <label htmlFor="">E-mail</label>
+              <input type="email" {...register('email')} />
+              <p>{errors?.email?.message}</p>
+            </InputContainer>
+
+            <InputContainer>
+              <label htmlFor="">Senha</label>
+              <input type="password" {...register('password')} />
+              <p>{errors?.password?.message}</p>
+            </InputContainer>
+
+            <InputContainer>
+              <label htmlFor="">Confirmar Senha</label>
+              <input type="password" {...register('confirmPassword')} />
+              <p>{errors?.confirmPassword?.message}</p>
+            </InputContainer>
+
+            <Button type="submit">Criar Conta</Button>
+          </Form>
+          <p>
+            Já possui conta ? <Link to="/login">Clique aqui.</Link>
+          </p>
+        </RightContainer>
+      </Container>
+    </div>
+  );
+}
