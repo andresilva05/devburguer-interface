af98fd45ee2d0950ff5afd728a0438f0d39e86ea
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:16:35 -0300

    feat: adicionar carrosséis de categorias e ofertas na página Home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,19 @@
+import { CategoriesCarousel } from '../../components/CategoriesCarousel';
+import { OffersCarousel } from '../../components/OffersCarousel';
+import { Banner, Container } from "./styles";
 
-
-export function Home () {
-    return (
+export function Home() {
+  return (
+    <main>
+      <Banner>
+        <h1>Bem-vindo(a)!</h1>
+      </Banner>
+      <Container>
         <div>
-            <h1>Home</h1>
+          <CategoriesCarousel/>
+          <OffersCarousel/>
         </div>
-    )
+      </Container>
+    </main>
+  );
 }
