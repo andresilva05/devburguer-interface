a8b99482a5907ea2c9679dba240d4d5468f49f21
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:15:57 -0300

    style: ajustar estilo do carrossel de ofertas e título

---
+++ src/components/OffersCarousel/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .carousel-item {
+    padding-right: 40px;
+  }
+  overflow-x: hidden;
+
+  .react-multi-carousel-list {
+    overflow: visible;
+  }
+  padding-left: 40px;
+  padding-bottom: 40px;
+`;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  font-weight: 800;
+  color: #61a120;
+  padding-bottom: 12px;
+  text-align: center;
+  margin: 70px 0;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 56px;
+    height: 4px;
+    background-color: #61a120;
+    margin: 12px auto 0;
+  }
+`;
