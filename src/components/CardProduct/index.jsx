6562a86d9ab86075cebae621f7beb4b7e701c379
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:12:31 -0300

    feat: criar componente CardProduct para exibição de produtos

---
+++ src/components/CardProduct/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { CardImage, Container } from './styles';
+import { CardButton } from '../CardButton';
+
+export function CardProduct({ product }) {
+  console.log(product);
+
+  return (
+    <Container>
+      <CardImage src={product.url} alt={product.name} />
+      <div>
+        <p> {product.name} </p>
+        <strong> {product.currencyValue} </strong>
+      </div>
+      <CardButton/>
+    </Container>
+  );
+}
+
+CardProduct.propTypes = {
+  product: PropTypes.object,
+};
