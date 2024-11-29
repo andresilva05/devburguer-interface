c8ab83ee0af6749458348f99b197009e42068133
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:24:32 -0300

    style: adicionar estilos para a tela de login e registro com fundo personalizado

--- src/containers/Register/styles.js
+++ src/containers/Register/styles.js
@@ -1,103 +1,84 @@
-import { Link as ReactLink} from 'react-router-dom';
+import { Link as ReactLink } from 'react-router-dom';
 import styled from 'styled-components';
 import BackgroundLogin from '../../assets/background-login.png';
 import Background from '../../assets/background.png';
 
 export const Container = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-
-`;
-
-export const LeftContainer = styled.div`
-    background:  url('${BackgroundLogin}');
-    background-size: cover;
-    background-position: center;
-
-    height: 100%;
-    width: 100%;
-    max-width: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    img{
-        width: 80%;
-    }
+  display: flex;
+  height: 100vh;
+  width: 100vw;
 `;
 
 export const RightContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-    height: 100%;
-    width: 100%;
-    max-width: 50%;
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
 
-    background:  url('${Background}');
-    background-color: #1E1E1E;
-    background-size: cover;
+  background: url('${Background}');
+  background-color: #1e1e1e;
+  background-size: cover;
 
-    p{
-        color: #ffff;
-        font-size: 18px;
-        font-weight: 800;
+  p {
+    color: #ffff;
+    font-size: 18px;
+    font-weight: 800;
 
-        a{
-            text-decoration: underline;
-            color: #ffff;
-        }
+    a {
+      text-decoration: underline;
+      color: #ffff;
     }
+  }
 `;
 
 export const Title = styled.h2`
- font-family: "Road Rage", sans-serif;
-    font-size: 40px;
-    color: #9758a6;
+  font-family: 'Road Rage', sans-serif;
+  font-size: 40px;
+  color: #9758a6;
 `;
 
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-    padding: 20px;
-    width: 100%;
-    max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding: 20px;
+  width: 100%;
+  max-width: 400px;
 `;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 100%;
+
+  input {
     width: 100%;
+    border: none;
+    height: 52px;
+    border-radius: 5px;
+    padding: 0 16px;
+  }
 
-    input {
-        width: 100%;
-        border: none;
-        height: 52px;
-        border-radius: 5px;
-        padding: 0 16px;
-    }
+  label {
+    font-size: 18px;
+    font-weight: 600;
+    color: #ffff;
+  }
 
-    label {
-        font-size: 18px;
-        font-weight: 600;
-        color: #ffff;
-
-    }
-
-    p{
-        font-size: 14px;
-        line-height: 80% ;
-        color: #cf3057;
-        font-weight: 600;
-        height: 10px;
-    }
+  p {
+    font-size: 14px;
+    line-height: 80%;
+    color: #cf3057;
+    font-weight: 600;
+    height: 10px;
+  }
 `;
 export const Link = styled(ReactLink)`
-    text-decoration: none;
-    color: #ffff;
-`
+  text-decoration: none;
+  color: #ffff;
+`;
