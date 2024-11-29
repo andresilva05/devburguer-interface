0d9cfeb72c07377e18e67560522e69e186b3c33d
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:19:33 -0300

    style: adicionar estilo ao banner e estrutura base para categorias e produtos

---
+++ src/containers/Menu/style.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import BannerHamburguer from '../../assets/BannerHamburguer.svg';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+export const Banner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 480px;
+  width: 100%;
+  position: relative;
+
+  background: url('${BannerHamburguer}');
+  background-color: #1f1f1f;
+  background-size: cover;
+  background-position: center;
+
+  h1 {
+    font-family: 'Road Rage', sans-serif;
+    font-size: 80px;
+    line-height: 65px;
+    color: #fff;
+    position: absolute;
+
+    right: 20%;
+    top: 30%;
+
+    span {
+      display: block;
+      color: #fff;
+      font-size: 20px;
+    }
+  }
+`;
+
+export const CategoryMenu = styled.div``;
+
+export const ProductsContainer = styled.div``;
