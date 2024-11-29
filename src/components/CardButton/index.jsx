9bf0dabadfe11f3b4df8c308b03ff52cc958d753
andresilva05 <andresilva9657@gmail.com>
2024-11-28 22:11:46 -0300

    feat: adicionar componente CardButton com ícone de carrinho

---
+++ src/components/CardButton/index.jsx
@@ -0,0 +1,10 @@
+import Cart from '../../assets/cart.svg';
+import { ContainerButton } from './styles';
+
+export function CardButton({ ...props }) {
+  return (
+    <ContainerButton {...props}>
+      <img src={Cart} alt="carrinho-de-compras" />
+    </ContainerButton>
+  );
+}
