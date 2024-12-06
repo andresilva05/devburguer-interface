eee0db3966541a31c60ad7e72e35b1c7a025ba0c
andresilva05 <andresilva9657@gmail.com>
2024-12-05 21:56:57 -0300

    Adiciona navegação de categorias com uso de react-router-dom no carousel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -2,56 +2,47 @@ import { useState, useEffect } from 'react';
 import { api } from '../../services/api';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import { Container, Title, ContainerItens } from './styles';
-
+import { Container, Title, ContainerItens, CategoryButton } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 export function CategoriesCarousel() {
-  const [categories, setCategories] = useState([]);
+	const [categories, setCategories] = useState([]);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    async function loadCategories() {
-      const { data } = await api.get('/categories');
+	useEffect(() => {
+		async function loadCategories() {
+			const { data } = await api.get('/categories');
+			setCategories(data);
+		}
+		loadCategories();
+	}, []);
 
-      setCategories(data);
-      console.log(data); // Corrigido: agora está logando a variável correta.
-    }
-    loadCategories();
-  }, []);
+	const responsive = {
+		superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 4 },
+		desktop: { breakpoint: { max: 3000, min: 1280 }, items: 4 },
+		tablet: { breakpoint: { max: 1280, min: 690 }, items: 3 },
+		mobile: { breakpoint: { max: 690, min: 0 }, items: 2 },
+	};
 
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 4,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1280 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 1280, min: 690 },
-      items: 3,
-    },
-    mobile: {
-      breakpoint: { max: 690, min: 0 },
-      items: 2,
-    },
-  };
-
-  return (
-    <Container>
-      <Title>Categorias</Title>
-      <Carousel
-        responsive={responsive}
-        infinite={true}
-        partialVisbile={false}
-        itemClass="carousel-item"
-      >
-        {categories.map((category) => (
-          <ContainerItens key={category.id} imageUrl= {category.url}>
-            <p>{category.name}</p>
-          </ContainerItens>
-        ))}
-      </Carousel>
-    </Container>
-  );
+	return (
+		<Container>
+			<Title>Categorias</Title>
+			<Carousel responsive={responsive} infinite={true} partialVisible={true} itemClass="carousel-item">
+				{categories.map((category) => (
+					<ContainerItens key={category.id} imageUrl={category.url}>
+						<CategoryButton
+							onClick={() =>
+								navigate({
+									pathname: '/cardapio',
+									search: `?categoria=${category.id}`,
+								})
+							}
+						>
+							{category.name}
+						</CategoryButton>
+					</ContainerItens>
+				))}
+			</Carousel>
+		</Container>
+	);
 }
