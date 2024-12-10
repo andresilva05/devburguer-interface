e693fcd5055aac0faec9a8c1f9a79b58767edb49
andresilva05 <andresilva9657@gmail.com>
2024-12-09 21:42:12 -0300

    feat: adicionar funcionalidade de carrinho ao componente CardProduct

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
 import { CardImage, Container } from './styles';
 import { CardButton } from '../CardButton';
+import { useCart } from '../../hooks/CartContext';
 
 export function CardProduct({ product }) {
-  return (
-    <Container>
-      <CardImage src={product.url} alt={product.name} />
-      <div>
-        <p> {product.name} </p>
-        <strong> {product.currencyValue} </strong>
-      </div>
-      <CardButton />
-    </Container>
-  );
+	const { putProductInCart } = useCart();
+	return (
+		<Container>
+			<CardImage src={product.url} alt={product.name} />
+			<div>
+				<p> {product.name} </p>
+				<strong> {product.currencyValue} </strong>
+			</div>
+			<CardButton onClick={() => putProductInCart(product)}>.</CardButton>
+		</Container>
+	);
 }
 
 CardProduct.propTypes = {
-  product: PropTypes.object,
+	product: PropTypes.object,
 };
