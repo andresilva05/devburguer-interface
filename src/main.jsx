c41f246b42c6f65d4deca30bf5c42c0b2b488bc0
andresilva05 <andresilva9657@gmail.com>
2024-12-22 13:10:12 -0300

    refactor: adicionar suporte a temas dinâmicos e reorganizar configuração do roteamento

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import { router } from './routes';
 
 import GlobaStyles from './styles/globalStyles';
 import AppProvider from './hooks';
 import { Elements } from '@stripe/react-stripe-js';
 import stripePromise from './config/stripeConfig';
+import { ThemeProvider } from 'styled-components';
+import { standardTheme } from './styles/themes/standard';
+import { Router } from './routes';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<AppProvider>
-			<Elements stripe={stripePromise}>
-				<RouterProvider router={router} />
-			</Elements>
-			<GlobaStyles />
-			<ToastContainer autoClose={2000} theme="dark" />
-		</AppProvider>
+		<ThemeProvider theme={standardTheme}>
+			<AppProvider>
+				<Elements stripe={stripePromise}>
+					<BrowserRouter>
+						<Router />
+					</BrowserRouter>
+				</Elements>
+				<GlobaStyles />
+				<ToastContainer autoClose={2000} theme="dark" />
+			</AppProvider>
+		</ThemeProvider>
 	</StrictMode>,
 );
