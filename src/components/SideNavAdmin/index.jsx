d06f656bf8c9bda92527f3630b0e317f3669ebf8
andresilva05 <andresilva9657@gmail.com>
2024-12-26 20:42:27 -0300

    feat: aprimorar componente SideNavAdmin com logo, links de navegação dinâmicos e funcionalidade de logout

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,7 +1,31 @@
-export function SideNavAdmin( ){
-    return (
-        <div>
-            <h2>Menu</h2>
-        </div>
-    )
+import Logo from '../../assets/logo-login.svg';
+import { navLinks } from './navLinks';
+import { SignOut } from '@phosphor-icons/react';
+import { Container, Footer, NavLink, NavLinkContainer } from './styles';
+import { useUser } from '../../hooks/UserContext';
+import { useResolvedPath } from 'react-router-dom';
+
+export function SideNavAdmin() {
+	const { logout } = useUser();
+	const { pathname } = useResolvedPath();
+
+	return (
+		<Container>
+			<img src={Logo} alt="Hamburguer Logo DevBurguer" />
+			<NavLinkContainer>
+				{navLinks.map((link) => (
+					<NavLink key={link.id} to={link.path} $isActive={pathname === link.path}>
+						{link.icon}
+						<span>{link.label}</span>
+					</NavLink>
+				))}
+			</NavLinkContainer>
+			<Footer>
+				<NavLink to="/login" onClick={logout}>
+					<SignOut to="/login" onClick={logout} />
+					<span>Sair</span>
+				</NavLink>
+			</Footer>
+		</Container>
+	);
 }
