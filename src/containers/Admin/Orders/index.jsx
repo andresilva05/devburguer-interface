06320c2980409bd034b05624981521ffc35e21c5
andresilva05 <andresilva9657@gmail.com>
2024-12-29 12:00:48 -0300

    feat: adicionado carregamento de pedidos da API e renderização em tabela
    - Implementado o carregamento dos pedidos usando a API e o estado 'orders'.
    - Adicionada tabela de pedidos utilizando componentes do Material-UI.
    - Adicionado o componente Row para renderizar cada linha da tabela.

--- src/containers/Admin/Orders/index.jsx
+++ src/containers/Admin/Orders/index.jsx
@@ -1,11 +1,45 @@
-
-
+import React, { useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Row } from './Row';
+import { api } from '../../../services/api';
 
 export function Orders() {
-    
-    return (
-        <div>
-            <h2>Pedidos</h2>
-        </div>
-    )
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const { data } = await api.get('orders');
+      setOrders(data);
+      console.log(data);  // Pode remover após verificar a resposta da API
+    }
+    loadOrders();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
